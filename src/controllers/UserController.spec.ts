83ef07b898592c535071b2a237185a6782d43bdc
dum2601 <doug_b_souza@hotmail.com>
2025-12-07 20:05:30 -0300

    primeiro desafio resolvido

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -5,7 +5,10 @@ import { makeMockResponse } from "../__mocks__/mockResponse.mock";
 
 describe('UserController', () => {
     const mockUserService: Partial<UserService> = {
-        createUser: jest.fn()
+        createUser: jest.fn(),
+        getAllUsers: jest.fn().mockReturnValue([
+            { name: "Nath", email: "nath@test.com" }
+        ])
     }
     
     const userController = new UserController(mockUserService as UserService);
@@ -18,8 +21,38 @@ describe('UserController', () => {
             }
         } as Request
         const mockResponse = makeMockResponse()
+        
         userController.createUser(mockRequest, mockResponse)
+
         expect(mockResponse.state.status).toBe(201)
         expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
+        expect(mockUserService.createUser).toHaveBeenCalledWith('Nath', 'nath@test.com')
+    })
+
+    it('Deve retornar erro caso o name não seja informado', () => {
+        const mockRequest = {
+            body: {
+                email: 'test@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        
+        userController.createUser(mockRequest, mockResponse)
+
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Name obrigatório' })
+    })
+
+    it('Deve chamar o service getAllUsers ao buscar todos os usuários', () => {
+        const mockRequest = {} as Request
+        const mockResponse = makeMockResponse()
+
+        userController.getAllUsers(mockRequest, mockResponse)
+
+        expect(mockUserService.getAllUsers).toHaveBeenCalled()
+        expect(mockResponse.state.status).toBe(200)
+        expect(mockResponse.state.json).toEqual([
+            { name: "Nath", email: "nath@test.com" }
+        ])
     })
 })
